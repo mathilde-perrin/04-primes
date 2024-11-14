4552dd674c427348d757be79d946d3a4f2f6a388
mathilde-perrin <mathilde.perrin@edu.esiee.fr>
2024-11-14 09:15:12 +0000

    message explicatif

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from math import sqrt
-
 #### Fonction secondaire
 
-
+"""
+On cherche à vérifier si un entier est premier ou pas.
+"""
 def isprime(p):
 
-    # votre code ici
+    """
+    Checke si p est un nombre premier.
 
-    pass
+    p est un int.
+
+    Retourne des booléens : Si p est premier retourne VRAI, si p n'est pas premier retouorne FAUX.
+    """
+    #Cas de base
+    if p<= 1:
+        return False
+    if p<= 3:
+        return True
+    if p%2==0 or p%3==0:
+        return False
+
+    #Vérification divisibles jusqu'à racine carrée de p
+    i = 5
+    while i*i <= p:
+        if p%i==0 or p%(i + 2)==0:
+            return False
+        i += 6
+    return True
 
 #### Fonction principale
 
-
 def main():
 
+    """
+    Appelle la fonction isprime(p) avec n = 100
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
